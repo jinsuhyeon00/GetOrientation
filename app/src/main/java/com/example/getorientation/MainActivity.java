73637cea035ac49e5d0be75b5b38c38db86e08f7
S-H JIN <jinsuhyeon00@naver.com>
2021-04-15 16:01:02 +0900

    compass with two sensors

--- app/src/main/java/com/example/getorientation/MainActivity.java
+++ app/src/main/java/com/example/getorientation/MainActivity.java
@@ -13,6 +13,9 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
   //  TextView txtAzimuth, txtPitch, txtRoll;
+
+    AzimuthView azimuthView;
+
     SensorManager sensorManager;
     Sensor magSensor, accSensor;
     SensorEventListener listener;
@@ -28,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
  //       txtPitch = findViewById(R.id.txtPitch);
 //        txtRoll = findViewById(R.id.txtRoll);
 
+        azimuthView = findViewById(R.id.azimuthView);
+
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         magSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
@@ -37,44 +42,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
                 switch (sensorEvent.sensor.getType()) {
-
                     case Sensor.TYPE_ACCELEROMETER:
-
-                        accValues = sensorEvent.values.clone(); break;
-
+                        accValues = sensorEvent.values.clone();
+                        break;
                     case Sensor.TYPE_MAGNETIC_FIELD:
-
-                        magValues = sensorEvent.values.clone(); break;
-
+                        magValues = sensorEvent.values.clone();
+                        break;
                 }
-
                 if (magValues != null && accValues != null) {
 
                     // 1) 회전(Rotation) 행렬과 경사(Inclination) 행렬 얻기
-
                     float[] R = new float[16]; //얻고자 하는 회전 행렬 (장비의 방향을 계산할 때 이용)
-
                     float[] I = new float[16]; //얻고자 하는 경사 행렬 (장비의 경사 각도를 계산할 때 이용)
-
                     SensorManager.getRotationMatrix(R, I, accValues, magValues);
-
-
                     // 2) 회전행렬로부터 방향 얻기
-
                     float[] values = new float[3];
-
                     SensorManager.getOrientation(R, values);
 
-
-                    if((int) radian2Degree(values[0]) == 180) {
-
-                        Toast.makeText(MainActivity.this, "180", Toast.LENGTH_SHORT);
+                   /* if((int) radian2Degree(values[0]) == 180 || radian2Degree(values[0]) == -180) {
 
                     } else if((int) radian2Degree(values[0]) == -180) {
 
                         Toast.makeText(MainActivity.this, "-180", Toast.LENGTH_SHORT);
 
-                    }
+                    }*/
+
+                    azimuthView.azimuth = (int) radian2Degree(values[0]);
+                    azimuthView.invalidate();
 
 
 //                    txtAzimuth.setText("Azimuth: " + (int) radian2Degree(values[0]));
